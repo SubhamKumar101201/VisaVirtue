543c6525171b1fa5fed5a77bfb5e4f590ac7f473
SubhamKumar101201 <das.subham101201@gmail.com>
2025-10-22 23:25:52 +0530

    changes in services page

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
+import VisaDestinations from '../components/VisaDestinations';
 
 export default function Services() {
   return (
-    <div className="p-10 text-center">
-      <h1 className="text-3xl font-bold text-[#780606]">Our Services</h1>
-      <p className="mt-4 text-gray-700">Details about visa consulting, documentation, and travel assistance.</p>
-    </div>
+    <VisaDestinations />
   );
 }
